4006bfa2b7d861bb44071cebc99fa1513a6abc60
md-nazim-Hossain <nazimhossaindpi@gmail.com>
2023-11-06 00:47:21 +0600

    fix: :bug: Fix the images path

--- src/data/index.ts
+++ src/data/index.ts
@@ -4,56 +4,56 @@ export const data: IImage[] = [
   {
     id: "0",
     checked: false,
-    src: "/assets/images/image-1.webp",
+    src: "./assets/images/image-1.webp",
   },
   {
     id: "1",
     checked: false,
-    src: "/assets/images/image-2.webp",
+    src: "./assets/images/image-2.webp",
   },
   {
     id: "2",
     checked: false,
-    src: "/assets/images/image-3.webp",
+    src: "./assets/images/image-3.webp",
   },
   {
     id: "3",
     checked: false,
-    src: "/assets/images/image-4.webp",
+    src: "./assets/images/image-4.webp",
   },
   {
     id: "4",
     checked: false,
-    src: "/assets/images/image-5.webp",
+    src: "./assets/images/image-5.webp",
   },
   {
     id: "5",
     checked: false,
-    src: "/assets/images/image-6.webp",
+    src: "./assets/images/image-6.webp",
   },
   {
     id: "6",
     checked: false,
-    src: "/assets/images/image-7.webp",
+    src: "./assets/images/image-7.webp",
   },
   {
     id: "7",
     checked: false,
-    src: "/assets/images/image-8.webp",
+    src: "./assets/images/image-8.webp",
   },
   {
     id: "8",
     checked: false,
-    src: "/assets/images/image-9.webp",
+    src: "./assets/images/image-9.webp",
   },
   {
     id: "9",
     checked: false,
-    src: "/assets/images/image-10.jpeg",
+    src: "./assets/images/image-10.jpeg",
   },
   {
     id: "10",
     checked: false,
-    src: "/assets/images/image-11.jpeg",
+    src: "./assets/images/image-11.jpeg",
   },
 ];
